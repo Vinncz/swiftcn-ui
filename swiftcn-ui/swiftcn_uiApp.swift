6d2fffc0aff31e1bd17c3402cd132d79c1f63bce
Vinncz <kevgun03@gmail.com>
2024-04-12 14:15:55 +0700

    Initial Commit

---
+++ swiftcn-ui/swiftcn_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftcn_uiApp.swift
+//  swiftcn-ui
+//
+//  Created by Vin on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftcn_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
